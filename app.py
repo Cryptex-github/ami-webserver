82f3ab61e08a94876092e69eec3e966db2e19dec
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 13:38:43 -0700

    oop

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ origins = [
 app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(api_router, prefix="/api")
 app.include_router(root_router)
-app.add_middleware(BaseHTTPMiddleware, auth)
+app.add_middleware(BaseHTTPMiddleware, dispatch=auth)
 app.add_middleware(CORSMiddleware, 
     allow_origins=origins,
     allow_credentials=True,
