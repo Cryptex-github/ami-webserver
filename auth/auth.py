8951f90e58c6b733c0bb9fcb028e7ce93674a19f
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 09:43:35 -0700

    Added auth folder

---
+++ auth/auth.py
@@ -0,0 +1,19 @@
+from fastapi import Request, Response
+from fastapi.responses import ORJSONResponse
+import os
+
+TOKEN = os.getenv("AUTH")
+
+async def auth(request, call_next):
+    if request.url.path in ["/", "/redoc", "/docs", "/openapi.json"]:
+        return await call_next(request)
+    
+    try:
+        token = request.headers["Authorization"]
+        if not token:
+            return ORJSONResponse({"message": "Missing authorization header"}, status_code=401)
+        
+        if token == TOKEN:
+            return await call_next(request)
+        
+        return ORJSONResponse({"message": "Invalid token"}, status_code=401)
