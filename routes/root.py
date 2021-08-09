8271e59ed022b9a5ac5659ae71684e6d64442386
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 10:51:50 -0700

    Add missing files

---
+++ routes/root.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+from werkzeug import safe_join
+
+root_router = APIRouter()
+
+
+@root_router.get("/")
+async def root():
+    return {"Hello": "World"}
+
+@root_router.get("/uploads/{filename}")
+async def uploads(filename: str):
+    file_path = safe_join("/app/uploads/", filename)
+    return FileResponse(path=file_path, filename=filename)
