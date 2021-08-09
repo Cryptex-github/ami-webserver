7f160fa414180180ee89be9db3f63ed1bb74c78e
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 13:56:37 -0700

    Move dotenv to app.py .

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
