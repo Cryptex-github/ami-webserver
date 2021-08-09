d8ffe850f47d02be03cab8b6f6b8b07b2c3c3211
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 10:44:52 -0700

    Add origins and hide doc

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from auth.auth import auth
 from routes.api import api_router
 from routes.root import root_router
 
+origins = [
+    "http://amidiscord.com",
+    "https://amidiscord.com",
+    "http://localhost:80",
+]
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(api_router, prefix="/api")
 app.include_router(root_router, prefix="/")
 app.add_middleware(BaseHTTPMiddleware, auth)
+app.add_middleware(CORSMiddleware, 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(HTTPSRedirectMiddleware)
 
