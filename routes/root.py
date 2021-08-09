9085c6fb4a9f9d5267bbe788b4cb0942ccb90fce
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-09 14:18:34 -0700

    Add website render

--- routes/root.py
+++ routes/root.py
@@ -2,15 +2,17 @@ import os
 
 UPLOAD_PATH = os.path.join(os.getcwd(), 'uploads')
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 from werkzeug.utils import safe_join
 
 root_router = APIRouter()
 
+templates = Jinja2Templates(directory="templates")
 
-@root_router.get("/")
-async def root():
-    return {"Hello": "World"}
+@root_router.get("/", response_class=HTMLResponse)
+async def root(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
 
 @root_router.get("/robots.txt")
 async def robots():
